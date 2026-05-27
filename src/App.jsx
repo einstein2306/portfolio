a21e2ddd45c7095a37e9691a4c2efcdc0b38725d
einstein2306 <pasinayaswanth2004@gmail.com>
2026-05-27 19:44:14 +0530

    added offcanvas to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -17,29 +17,35 @@ function App() {
         <nav className="navbar navbar-expand-lg navbar-light shadow shadow-sm">
           <div className='container-fluid'>
               <NavLink className="navbar-brand" to="#">Yaswanth</NavLink>
-              <button className="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="collapse" 
+              <button className="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="offcanvas" 
               data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon outline-none"></span>
               </button>
-              <div className="collapse navbar-collapse justify-content-start" id="navbarSupportedContent">
-                <ul className="navbar-nav ms-auto">
-                  <li className="nav-item">
-                    <NavLink className={({isActive}) => isActive ? "nav-link nav-active" : "nav-link"} to='/'>Home</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className={({isActive}) => isActive ? "nav-link nav-active" : "nav-link"} to='/About'>About</NavLink>
-                  </li>
+              <div className="offcanvas offcanvas-start" tabIndex="-1" id="navbarSupportedContent">
+                <div className='offcanvas-header'>
+                  <h5>Menu</h5>
+                  <button className='btn-close border-0 shadow-none' data-bs-dismiss="offcanvas"></button>
+                </div>
+                  <div className='offcanvas-body'>
+                      <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                          <NavLink className={({isActive}) => isActive ? "nav-link nav-active" : "nav-link"} to='/'>Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                          <NavLink className={({isActive}) => isActive ? "nav-link nav-active" : "nav-link"} to='/About'>About</NavLink>
+                        </li>
 
-                  <li className="nav-item">
-                    <NavLink className={({isActive}) => isActive ? "nav-link nav-active" : "nav-link"} to='/projects'>Projects</NavLink>
-                  </li>
-                   <li className="nav-item">
-                    <NavLink className={({isActive}) => isActive ? "nav-link nav-active" : "nav-link"} to='/experience'>Experience</NavLink>
-                  </li>
-                   <li className="nav-item">
-                    <NavLink className={({isActive}) => isActive ? "nav-link nav-active" : "nav-link"} to='/certifications'>Certifications</NavLink>
-                  </li>
-                </ul>
+                        <li className="nav-item">
+                          <NavLink className={({isActive}) => isActive ? "nav-link nav-active" : "nav-link"} to='/projects'>Projects</NavLink>
+                        </li>
+                        <li className="nav-item">
+                          <NavLink className={({isActive}) => isActive ? "nav-link nav-active" : "nav-link"} to='/experience'>Experience</NavLink>
+                        </li>
+                        <li className="nav-item">
+                          <NavLink className={({isActive}) => isActive ? "nav-link nav-active" : "nav-link"} to='/certifications'>Certifications</NavLink>
+                        </li>
+                      </ul>
+                    </div>
               </div>
           </div>
       </nav>  
